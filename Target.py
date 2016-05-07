be5885fd7705854c169487b97659ccd52c9f85f1
Pablo Fernandez <contact@pablofernandez.com>
2016-05-07 16:02:09 -0700

    Added files via upload

---
+++ Target.py
@@ -0,0 +1,24 @@
+class Target:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.steps = 25/3
+
+    def render(self):
+        star = loadImage("Star.png")       
+        image(star, self.x,self.y)
+
+                
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+    def getR(self):
+        return 30
+    
+    def score(self):
+        print("Score")
+        """ Nothing needed here right now """
+        
